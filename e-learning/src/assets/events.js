fab2b829aab764014d8eab65f2d6bffda49947e5
TobyMike-max <osinubi.michael@gmail.com>
2023-08-31 23:54:10 +0100

    [Create]: New file created

---
+++ e-learning/src/assets/events.js
@@ -0,0 +1,24 @@
+const events = [
+	{
+		timestamp: '10:00 AM',
+		title: 'Meeting',
+		content: 'New learning formats',
+	},
+	{
+		timestamp: '11:00 AM',
+		title: 'Lecture',
+		content: 'Wed Design Trends',
+	},
+	{
+		timestamp: '2:00 PM',
+		title: 'Lesson',
+		content: 'JavaScript Features',
+	},
+	{
+		timestamp: '4:30 PM',
+		title: 'Exam',
+		content: 'JavaScript',
+	}
+];
+
+export default events;
