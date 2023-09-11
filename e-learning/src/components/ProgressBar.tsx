54b4f505141b4c15d87c3a7d99a960c2599835e4
TobyMike-max <osinubi.michael@gmail.com>
2023-09-11 13:23:15 +0100

    Progress bar component

---
+++ e-learning/src/components/ProgressBar.tsx
@@ -0,0 +1,13 @@
+const ProgressBar = ({ progress }) => {
+	console.log(progress);
+    return (
+	<div className="h-2 w-36 bg-gray-300 rounded-full mr-2">
+	  <div 
+	  style={{ width: `${progress}%`}} 
+	  className={`h-full w-full rounded-full ${progress < 70 ? "bg-[#df6690]" : "bg-green-600"}`}>
+	  </div>
+        </div>
+    );
+};
+
+export default ProgressBar;
