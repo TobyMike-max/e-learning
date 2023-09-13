6573a795bf70e0aaaf8d7978dbd00275dda94492
TobyMike-max <osinubi.michael@gmail.com>
2023-09-13 22:19:52 +0100

    Redo main and App files

--- e-learning/src/App.tsx
+++ e-learning/src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Dashboard from './pages/Dashboard'
+import { useState, useEffect } from 'react';
+import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
-	<div>
-	  <Dashboard />
-	</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="*" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
