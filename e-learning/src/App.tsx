89d128e132faa535a5c3133ef18f646abaf77212
TobyMike-max <osinubi.michael@gmail.com>
2023-09-15 09:54:47 +0100

    Main app update

--- e-learning/src/App.tsx
+++ e-learning/src/App.tsx
@@ -1,8 +1,17 @@
 import { useState, useEffect } from 'react';
 import Dashboard from './pages/Dashboard';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useHistory } from 'react-router-dom';
 
 export default function App() {
+
+	{/*const history = useHistory();
+
+	useEffect(() => {
+		if (window.location.pathname !== '/dashboard') {
+			history.push('/dashboard');
+		}
+		}, [history]);*/}
+
   return (
     <Router>
       <Routes>
