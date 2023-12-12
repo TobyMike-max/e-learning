5f810334b3c5bed9389ea8d91602b78998c97b33
TobyMike-max <osinubi.michael@gmail.com>
2023-12-12 03:25:41 +0100

    Cookies fix

--- pg_server/controller/auth.js
+++ pg_server/controller/auth.js
@@ -89,7 +89,7 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
 	res.clearCookie("accessToken", {
-		sameSite: "none", secure
+		sameSite: "none", secure: "true",
 	}).status(200).json("User has been logged out")
 		
 }
