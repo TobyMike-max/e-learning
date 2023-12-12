dc4294317a98ce56f3ef42645f09b9335a6fbeec
TobyMike-max <osinubi.michael@gmail.com>
2023-12-12 01:55:00 +0100

    Changed database connection code

--- pg_server/connect.js
+++ pg_server/connect.js
@@ -1,13 +1,22 @@
-import pg from 'pg'
+import pkg from 'pg'
 import dotenv from 'dotenv'
 
 dotenv.config();
-const Pool = pg.Pool;
+const { Pool } = pkg
+export const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false
+	}
+});
 
+/**
+ * const pgp = pg();
+export const pool = pgp(process.env.DATABASE_URL);
 export const pool = new Pool({
 	host: process.env.DATABASE_HOST,
 	user: process.env.DATABASE_USER,
 	password: process.env.DATABASE_PASSWORD,
 	port: process.env.DATABASE_PORT,
 	database: process.env.DATABASE_NAME
-})
+}) **/
